0266982847e23711af53aaf2e975c3e1be556827
smj06 <smj199861@naver.com>
2022-03-30 13:02:05 +0900

    first commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -9,28 +9,18 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-import os
-from pathlib import Path
 
-from os import environ
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# reading .env file
-environ.Env.read_env(
-    env_file= os.path.join(BASE_DIR, '.env')
-)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ihne)!vvukh4#_xw7o*iduzyt=-%)*(-x&usg%@!xwug2zqkgc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
